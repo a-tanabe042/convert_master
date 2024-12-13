178ba83076edc4e0281a90323f7e65bc9c4938cc
Akira <akira.inaji.0716@gmail.com>
2024-12-13 11:59:44 +0900

    MAUIの新規プロジェクト

---
+++ FileConverter/FileConverter/Controller/WindowsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileConverter.Controller
+{
+    public class WindowsController
+    {
+        public WindowsController() { }
+
+        public bool ImportInputFile(string fileName)
+        {
+            Debug.WriteLine("ファイル読み込みメソッド");
+            return false;
+        }
+    }
+}
---
+++ FileConverter/FileConverter/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using FileConverter.Controller;
+
+namespace FileConverter
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+#if WINDOWS
+            // Windows独自の処理
+            WindowsController windowsCotroller = new WindowsController();
+            windowsCotroller.ImportInputFile("");
+
+#elif MACCATALYST
+            // Mac独自の処理
+
+#endif
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+
+}
